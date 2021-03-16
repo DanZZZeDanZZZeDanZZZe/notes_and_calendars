4309759f468f07e4e49231c4fbed7f74f5badffe
Artem Grankin <artem_grankin@epam.com>
2021-03-16 11:06:19 +0300

    feature: add login page

---
+++ client/src/components/PageWrapper/styled-components.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import constants from '../../constants'
+
+const { colors, shadows, breakpoints, sizes } = constants.css
+
+const Wrapper = styled.main`
+  box-sizing: border-box;
+  width: 100%;
+  height: 100vh;
+  background-color: ${colors.OCEAN_GREEN};
+  @media only screen and (max-width: ${breakpoints.SM_SIZE}) {
+  }
+  @media only screen and (min-width: ${breakpoints.SM_SIZE}) {
+    padding: 0 ${sizes.SIDE_MENU_SMALL_WIDTH};
+  }
+  @media only screen and (min-width: ${breakpoints.LG_SIZE}) {
+    padding: 0 ${sizes.SIDE_MENU_BIG_WIDTH};
+  }
+`
+const ContentSpace = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  min-height: 100%;
+  max-height: 100%;
+  overflow-y: auto;
+  background-color: ${colors.GHOST_WHITE};
+  box-shadow: ${shadows.MATERIAL_3};
+`
+
+export { Wrapper, ContentSpace }
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,12 @@
+import LoginForm from '../components/forms/LoginForm'
+import PageWrapper from '../components/PageWrapper'
+
+function LoginPage(props) {
+  return (
+    <PageWrapper>
+      <LoginForm />
+    </PageWrapper>
+  )
+}
+
+export default LoginPage
