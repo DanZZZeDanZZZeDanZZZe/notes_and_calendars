068c8372071dde689ba3ec214dbf42f7ebd371c2
Artem Grankin <artem_grankin@epam.com>
2021-03-23 19:52:56 +0300

    chore

--- client/src/utils/localStorageService.js
+++ client/src/utils/localStorageService.js
@@ -1,34 +1,32 @@
-class LocalStorageService {
-  constructor() {
-    this.storage = window.localStorage
-  }
+const storage = window.localStorage
 
+const localStorageService = {
   getItem(name) {
-    return JSON.parse(this.storage.getItem(name))
-  }
+    return JSON.parse(storage.getItem(name))
+  },
 
   setItem(name, value) {
-    this.storage.setItem(name, JSON.stringify(value))
-  }
+    storage.setItem(name, JSON.stringify(value))
+  },
 
   removeItem(name) {
-    this.storage.removeItem(name)
-  }
+    storage.removeItem(name)
+  },
 
   getItems(arr) {
     return arr.reduce(
       (acc, name) => ({ ...acc, [name]: this.getItem(name) }),
       {}
     )
-  }
+  },
 
   setItems(arr) {
     arr.forEach(([name, value]) => this.setItem(name, value))
-  }
+  },
 
   removeItems(arr) {
     arr.forEach((name) => this.removeItem(name))
-  }
+  },
 }
 
-export default LocalStorageService
+export default localStorageService
